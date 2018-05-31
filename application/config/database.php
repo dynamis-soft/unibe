c0659a78461fdfe727468476f95d749fb58748b7
katat <kataty89@gmail.com>
2018-05-31 10:00:01 -0600

    Agregue constant de urls

--- application/config/database.php
+++ application/config/database.php
@@ -73,12 +73,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-$db['default'] = array(
+/*$db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
 	'username' => 'root',
 	'password' => 'root',
-	'database' => 'unibe',
+	'database' => 'ifecr',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
@@ -93,4 +93,26 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
+);*/
+$db['default'] = array(
+    'dsn' => '',
+    'hostname' => 'DESKTOP-D35BTDR\SQLEXPRESS',
+    'username' => 'ClinicaCRM',
+    'port' => 1433,
+    'password' => 'Cl1n1c4CRM',
+    'database' => 'unibe',
+    'dbdriver' => 'sqlsrv',
+    'dbprefix' => '',
+    'pconnect' => TRUE,
+    'db_debug' => TRUE,
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
 );
